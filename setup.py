017d22f085142fec8966d9e28ed87481ae5bd868
Roberto Preste <roberto.preste@gmail.com>
2019-04-19 11:59:44 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(  # pragma: no cover
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/robertopreste/apyhgnc",
-    version="0.1.0",
+    version="version='0.1.1'",
     zip_safe=False,
 )
