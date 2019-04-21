67737b76186f2c8301d539157c64bb1701384b4b
Roberto Preste <roberto.preste@gmail.com>
2019-04-21 11:18:09 +0200

    Fix importing issue

--- apyhgnc/__init__.py
+++ apyhgnc/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by Roberto Preste
-
+from apyhgnc.classes import Info, Fetch, Search
 __author__ = """Roberto Preste"""
 __email__ = "robertopreste@gmail.com"
 __version__ = '0.1.2'
