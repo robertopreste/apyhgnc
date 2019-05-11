a9a627fef43113906c96eba5c470c3a4f77a5f90
Roberto Preste <roberto.preste@gmail.com>
2019-05-11 17:32:13 +0200

    Update tests and testfiles

--- tests/test_Info.py
+++ tests/test_Info.py
@@ -25,7 +25,7 @@ class TestInfo:
         assert result == expect
 
     def test_numDoc(self):
-        expect = 42969
+        expect = 42970
         result = self.i.numDoc
 
         assert result == expect
